09d533fa45b94f040f1761b17e02a732e8e70c17
DuyAnhDz06 <106655395+DuyAnhDz06@users.noreply.github.com>
2022-10-10 09:10:51 +0700

    API 10/10/2022 ok

--- demo/api/app.js
+++
@@ -1,9 +0,0 @@
-import fetch from 'node-fetch'
-
-getAPI()
-
-function getAPI () {
-  fetch('http://localhost:6969/api')
-    .then(response => response.json())
-    .then(data => (document.body.innerHTML = data))
-}
--- lab8/app.js
+++ lab8/app.js
@@ -1,17 +1,51 @@
+//module import
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const path = require("path");
 
+//database connect + access
 const mongoose = require("mongoose");
 const studentModel = require("./models/StudentSchema");
-const url = "mongodb+srv://duyanhdz:123456daica@cluster0.kqbu68q.mongodb.net/";
+//url connection string (local db)
+//const url = "mongodb://localhost:27017/greenwich";
+//url connection string (cloud db)
+const url =
+  "mongodb+srv://longndt:xdHB48B3Fv*%40TPa@cluster0.i5kings.mongodb.net/greenwich";
+//Note: "greenwich" là tên của database
 mongoose.connect(url, { useNewUrlParser: true }, (err) => {
   if (err) {
     console.log(err);
   } else {
     console.log("connect to db succeed !");
   }
+});
+
+//body-parser: get form input
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//render ra form add
+app.get("/add", (req, res) => {
+  res.render("add");
+});
+
+//nhận và xử lý dữ liệu từ form add
+app.post("/add", (req, res) => {
+  //console.log(req.body);
+  //res.send(req.body);
+  //res.render("output", { student: req.body });
+
+  //tạo object student chứa dữ liệu nhập từ form
+  var student = new studentModel(req.body);
+  //lưu object student vào database
+  student.save((err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.redirect("/student");
+    }
+  });
 });
 
 app.set("views", path.join(__dirname, "views"));
@@ -25,8 +59,19 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.get("/student", function (req, res) {
-  res.render("student");
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", (req, res) => {
+  var username = req.body.username;
+  var password = req.body.password;
+  var check = "Login failed !";
+  if (username == "admin" && password == "123456") {
+    check = "Login succeed !";
+  }
+  //res.send(check);
+  res.render("check", { result: check });
 });
 
 app.get("/student", (req, res) => {
